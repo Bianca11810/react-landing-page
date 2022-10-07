cb8898a71fc6147d2986e0d6ebc438fc16b7c918
Bianca11810 <Bianca11810@gmail.com>
2022-10-07 01:22:11 +0000

    My first commit

---
+++ src/js/component/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const footer = () => {
+	return (
+        <div className="footer">
+            <div style={{color: 'white'}}>
+                <p>Copyright something something</p>
+            </div>
+        </div>
+		
+	);
+};
+
+export default footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 
+import Welcome from "./welcome";
+import Nav from './nav';
+import Card from './card';
+import Footer from './footer';
+
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<Nav/>
+			<Welcome/> 
+			<div className="row">
+				<div className="col-3">
+					<Card/>
+				</div>
+				<div className="col-3">
+					<Card/>
+				</div>
+				<div className="col-3">
+					<Card/>
+				</div>
+				<div className="col-3">
+					<Card/>
+				</div>
+			</div>
+			<Footer/>
 		</div>
 	);
 };
